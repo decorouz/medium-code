5507e9b21b569b2a0c3a5eac90489467255599f5
Biola <abiola2326@gmail.com>
2020-07-13 00:51:48 +0100

    react table7 example

--- table-example/src/App.js
+++ table-example/src/App.js
@@ -1,7 +1,42 @@
 import React, { useEffect, useState, useMemo } from 'react'
+import {
+  Container,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+} from 'reactstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import dataService from './services/data'
 import TableContainer from './components/TableContainer'
+import { SelectColumnFilter } from './filter'
+
+const renderRowSubComponent = (row) => {
+  const {
+    name: { first, last },
+    location: { city, street, postcode },
+    picture,
+    cell,
+  } = row.original
+
+  return (
+    <Card style={{ width: '18rem', margin: '0 auto' }}>
+      <CardImg top src={picture.large} alt="Card image cap" />
+      <CardBody>
+        <CardTitle>
+          <strong>{`${first} ${last}`}</strong>
+        </CardTitle>
+        <CardText>
+          <strong>Phone</strong>: {cell}
+          <br />
+          <strong>Address</strong>
+          {`${street.name} ${street.number} - ${postcode} - ${city}`}
+        </CardText>
+      </CardBody>
+    </Card>
+  )
+}
 
 function App() {
   const [data, setData] = useState([])
@@ -21,18 +56,61 @@ function App() {
   //Define Columns
   const columns = useMemo(
     () => [
-      { Header: 'Title', accessor: 'name.title' },
+      {
+        Header: () => null,
+        id: 'expander',
+        Cell: ({ row }) => (
+          <span {...row.getToggleRowExpandedProps()}>
+            {row.isExpanded ? '👇' : '👉'}
+          </span>
+        ),
+      },
+      { Header: 'Title', accessor: 'name.title', Filter: SelectColumnFilter },
       { Header: 'First Name', accessor: 'name.first' },
       { Header: 'Last Name', accessor: 'name.last' },
       { Header: 'Email', accessor: 'email' },
       { Header: 'City', accessor: 'location.city' },
+      {
+        Header: 'Hemisphere',
+        accessor: (values) => {
+          const { latitude, longitude } = values.location.coordinates
+          const first = Number(latitude) > 0 ? 'N' : 'S'
+          const second = Number(longitude) > 0 ? 'E' : 'W'
+          return first + '/' + second
+        },
+        disableSortBy: true,
+        Filter: SelectColumnFilter,
+        filter: 'equal',
+        Cell: ({ cell }) => {
+          const { value } = cell
+
+          const pickEmoji = (value) => {
+            let first = value[0]
+            let second = value[2]
+            const options = ['⇖', '⇗', '⇙', '⇘']
+            let num = first === 'N' ? 0 : 2
+            num = second === 'E' ? num + 1 : num
+            return options[num]
+          }
+
+          return (
+            <div style={{ textAlign: 'center', fontSize: 18 }}>
+              {pickEmoji(value)}
+            </div>
+          )
+        },
+      },
     ],
     []
   )
 
   return (
     <Container style={{ marginTop: 100 }}>
-      <TableContainer columns={columns} data={data} />
+      <TableContainer
+        columns={columns}
+        data={data}
+        renderRowSubComponent={renderRowSubComponent}
+      />
     </Container>
   )
 }
--- table-example/src/components/TableContainer.jsx
+++ table-example/src/components/TableContainer.jsx
@@ -1,45 +1,186 @@
-import React from 'react'
-import { useTable } from 'react-table'
-import { Table } from 'reactstrap'
+import React, { Fragment } from 'react'
+import {
+  useTable,
+  useFilters,
+  useSortBy,
+  useExpanded,
+  usePagination,
+} from 'react-table'
+import { Table, Row, Col, Button, Input, CustomInput } from 'reactstrap'
+
+import { Filter, DefaultColumnFilter } from '../filter'
 
 //Use table requires an object with 2 properties: data(contact) and column.
 
-const TableContainer = ({ columns, data }) => {
+const TableContainer = ({ columns, data, renderRowSubComponent }) => {
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
+    // rows, we change 'rows to page( which keeps rows from an active page)
+    page,
     prepareRow,
-  } = useTable({ columns, data })
+    visibleColumns,
+    // below new props related to 'usePagination' hook
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn: { Filter: DefaultColumnFilter },
+      initialState: { pageIndex: 0, pageSize: 10 },
+    },
+    useFilters,
+    useSortBy,
+    useExpanded,
+    usePagination
+  )
 
-  console.log(useTable({ columns, data }))
+  const generateSortingIndicatior = (column) => {
+    return column.isSorted ? (column.isSortedDesc ? ' 🔽' : ' 🔼') : ''
+  }
+
+  const onChangeInSelect = (event) => {
+    console.log(Number(event.target.value))
+    setPageSize(Number(event.target.value))
+  }
+
+  const onChangeInput = (event) => {
+    const page = event.target.value ? Number(event.target.value) - 1 : 0
+    gotoPage(page)
+  }
 
   return (
-    <Table {...getTableProps()}>
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-            ))}
-          </tr>
-        ))}
-      </thead>
+    <Fragment>
+      <Table bordered hover {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                //before sortby
+                // <th {...column.getHeaderProps()}>{column.render('Header')}</th>
 
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-              })}
+                // After sortBy
+                // <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                //   {column.render('Header')}
+                //   {generateSortingIndicatior(column)}
+                // </th>
+
+                //After connecting useFilter
+                <th {...column.getHeaderProps()}>
+                  <div {...column.getSortByToggleProps()}>
+                    {column.render('Header')}
+                    {generateSortingIndicatior(column)}
+                  </div>
+                  <Filter column={column} />
+                </th>
+              ))}
             </tr>
-          )
-        })}
-      </tbody>
-    </table>
+          ))}
+        </thead>
+
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row)
+            return (
+              // Before implemment Sub Component
+
+              // <tr {...row.getRowProps()}>
+              //   {row.cells.map((cell) => {
+              //     return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              //   })}
+              // </tr>
+
+              <Fragment key={row.getRowProps().key}>
+                <tr>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                    )
+                  })}
+                </tr>
+                {row.isExpanded && (
+                  <tr>
+                    <td colSpan={visibleColumns.length}>
+                      {renderRowSubComponent(row)}
+                    </td>
+                  </tr>
+                )}
+              </Fragment>
+            )
+          })}
+        </tbody>
+      </Table>
+
+      <Row style={{ maxWidth: 1000, margin: '0 auto', textAlign: 'center' }}>
+        <Col md={3}>
+          <Button
+            color="primary"
+            onClick={() => gotoPage(0)}
+            disabled={!canPreviousPage}
+          >
+            {'<<'}
+          </Button>
+          <Button
+            color="primary"
+            onClick={previousPage}
+            disabled={!canPreviousPage}
+          >
+            {'<'}
+          </Button>
+        </Col>
+        <Col md={2} style={{ marginTop: 7 }}>
+          Page{' '}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>
+        </Col>
+        <Col md={2}>
+          <Input
+            type="number"
+            min={1}
+            style={{ width: 70 }}
+            max={pageOptions.length}
+            defaultValue={pageIndex + 1}
+            onChange={onChangeInput}
+          />
+        </Col>
+        <Col md={2}>
+          <CustomInput
+            id="custom-select"
+            type="select"
+            value={pageSize}
+            onChange={onChangeInSelect}
+          >
+            {[10, 20, 30, 40, 50].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                Show {pageSize}
+              </option>
+            ))}
+          </CustomInput>
+        </Col>
+        <Col md={3}>
+          <Button color="primary" onClick={nextPage} disabled={!canNextPage}>
+            {'>'}
+          </Button>
+          <Button
+            color="primary"
+            onClick={() => gotoPage(pageCount - 1)}
+            disabled={!canNextPage}
+          >
+            {'>>'}
+          </Button>
+        </Col>
+      </Row>
+    </Fragment>
   )
 }
 export default TableContainer
